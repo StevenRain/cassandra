52c7f942561fe3709530c3707b4d54f04ccc2bd3
woody-jx <woody-jx@jxinternet.com>
2018-07-05 09:32:07 +0800

    add

--- src/main/java/com/cassandra/service/BetService.java
+++ src/main/java/com/cassandra/service/BetService.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 @Slf4j
 public class BetService {
-
     public static void  betByHistory(){
+        String curentGameIssueNo = S118Utils.getLatestGameIssueNumber();
         List<OpenResult.OpenResultDto> openResultDtoList = S118Utils.buildLatestOpenResult().getOpenResultDtoList();
         openResultDtoList.forEach(openResultDto -> log.info("{}", openResultDto));
         //倒数第二
@@ -19,38 +19,81 @@ public class BetService {
         //倒数第一
         OpenResult.OpenResultDto lastResult = openResultDtoList.get(openResultDtoList.size() - 1);
 
+        if (Long.valueOf(curentGameIssueNo)  - Long.valueOf(lastResult.getGameIssueNo())==1){
+            log.info("投注的期号相同不投注");
+            return;
+        }
+        log.info("投注开始.....");
 
         //单双连续
         if (lastResult.getOddOrEven().equals(openResultTow.getOddOrEven())){
             //单双都投注 调用投注的接口
-             S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),WoodyUtils.getToken(PublicUtils.EMAIL)));
+             S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
         }else {
             //投注单跳
             if(lastResult.getOddOrEven().equals(PublicUtils.ODD)){
                  lastResult.setOddOrEven(PublicUtils.EVEN);
-                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),WoodyUtils.getToken(PublicUtils.EMAIL)));
+                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
             }else {
                  lastResult.setOddOrEven(PublicUtils.ODD);
-                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),WoodyUtils.getToken(PublicUtils.EMAIL)));
+                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
             }
 
         }
         //大小连续
         if (lastResult.getBigOrSmall().equals(openResultTow.getBigOrSmall())){
-            S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getBigOrSmall(),WoodyUtils.getToken(PublicUtils.EMAIL)));
+            S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getBigOrSmall(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
         }else {
             //投注单跳
             if(lastResult.getBigOrSmall().equals(PublicUtils.BIG)){
-                lastResult.setOddOrEven(PublicUtils.SMALL);
-                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getBigOrSmall(),WoodyUtils.getToken(PublicUtils.EMAIL)));
+                lastResult.setBigOrSmall(PublicUtils.SMALL);
+                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getBigOrSmall(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
             }else {
-                lastResult.setOddOrEven(PublicUtils.BIG);
-                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),WoodyUtils.getToken(PublicUtils.EMAIL)));
+                lastResult.setBigOrSmall(PublicUtils.BIG);
+                 S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
             }
         }
 
 
-        log.info("本期{},没有投注,上一期的期号是{}",S118Utils.getLatestGameIssueNumber(),lastResult.getGameIssueNo());
+        log.info("本期{},上一期的期号是{}",S118Utils.getLatestGameIssueNumber(),lastResult.getGameIssueNo());
+    }
+    public static void betOdd(){
+        String curentGameIssueNo = S118Utils.getLatestGameIssueNumber();
+        List<OpenResult.OpenResultDto> openResultDtoList = S118Utils.buildLatestOpenResult().getOpenResultDtoList();
+        openResultDtoList.forEach(openResultDto -> log.info("{}", openResultDto));
+        //倒数第二
+        OpenResult.OpenResultDto openResultTow = openResultDtoList.get(openResultDtoList.size() - 2);
+        //倒数第一
+        OpenResult.OpenResultDto lastResult = openResultDtoList.get(openResultDtoList.size() - 1);
+
+        if (Long.valueOf(curentGameIssueNo)  - Long.valueOf(lastResult.getGameIssueNo())==1){
+            log.info("投注的期号相同不投注");
+            return;
+        }
+        //单双连续
+        if (lastResult.getOddOrEven().equals(openResultTow.getOddOrEven())){
+            //单双都投注 调用投注的接口
+            S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
+        }else {
+            log.info("");
+        }
+    }
+    //单双  大小  连续
+    public static void betOnlyOdd(){
+        String curentGameIssueNo = S118Utils.getLatestGameIssueNumber();
+        List<OpenResult.OpenResultDto> openResultDtoList = S118Utils.buildLatestOpenResult().getOpenResultDtoList();
+        openResultDtoList.forEach(openResultDto -> log.info("{}", openResultDto));
+        //倒数第二
+        OpenResult.OpenResultDto openResultTow = openResultDtoList.get(openResultDtoList.size() - 2);
+        //倒数第一
+        OpenResult.OpenResultDto lastResult = openResultDtoList.get(openResultDtoList.size() - 1);
+
+        if (Long.valueOf(curentGameIssueNo)  - Long.valueOf(lastResult.getGameIssueNo())==1){
+            log.info("投注的期号相同不投注");
+            return;
+        }
+        S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getOddOrEven(),PublicUtils.PRICE2,WoodyUtils.getToken(PublicUtils.EMAIL)));
+        S118Utils.bet( WoodyUtils.buildBetByNumber(lastResult.getBigOrSmall(),PublicUtils.PRICE,WoodyUtils.getToken(PublicUtils.EMAIL)));
     }
 
 }
--- src/main/java/com/cassandra/task/UpdateOpenCodeForWoodyTask.java
+++ src/main/java/com/cassandra/task/UpdateOpenCodeForWoodyTask.java
@@ -3,6 +3,7 @@ package com.cassandra.task;
 
 import com.cassandra.service.BetService;
 import com.cassandra.utils.S118Utils;
+import com.cassandra.utils.WoodyUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,16 +12,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class UpdateOpenCodeForWoodyTask {
-
-    private final String token="d2b30fca9a00f008cc8af877baeeef8c";
-    private final String userName="guest0080233";
-    private final String psw="123456qaz";
-
-
-//    @Scheduled(cron = "5/10 * * * * *")
+    @Scheduled(cron = "5/10 * * * * *")
     public void updateOpenCodeWoody() {
-        log.info("投注之前的金额：{}",S118Utils.getBalance(token));
-        BetService.betByHistory();
-        log.info("投注之后金额：{}",S118Utils.getBalance(token));
+        log.info("投注之前的金额：{}",S118Utils.getBalance(WoodyUtils.getToken("163.com")));
+        //BetService.betByHistory();
+        //BetService.betOdd();
+        //这位老司机稳
+        BetService.betOnlyOdd();
+        log.info("投注之后金额：{}",S118Utils.getBalance(WoodyUtils.getToken("163.com")));
     }
 }
